e460d1adbab135ca733d27ae660b1d51983d022b
Iñigo Sáez Casares <inigo.saez@unimi.it>
2026-01-25 15:33:49 +0100

    Update input files.

--- scripts/run_leonardo_ac.sh
+++ scripts/run_leonardo_ac.sh
@@ -6,7 +6,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-task=1
 
-#SBATCH --job-name=nc_ps_n64_kf0.27_x1_z_0.1	
+#SBATCH --job-name=nc_ps_cnn_n64_x1_z_0.1_tr
+##SBATCH --job-name=nc_ps_n32_x1_z_0.1
 #SBATCH --err=/leonardo_work/IscrC_GraphMLS/isaezcas/logs/error/%x.%j.err
 #SBATCH --out=/leonardo_work/IscrC_GraphMLS/isaezcas/logs/output/%x.%j.out
 #SBATCH --account=IscrC_GraphMLS
@@ -19,13 +20,21 @@ n_threads=1
 
 ########## Param. file #########
 
+## NC
+
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_xlum_0.3_z_0.1.toml"
+
 ## CNN
 
 #param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_cnn_o_n64_xlum_0.3_z_0.1.toml"
-#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_cnn_o_n64_xlum_0.3_z_0.1.toml"
+param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_cnn_o_n64_xlum_0.3_z_0.1.toml"
+
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_kf0.33_cnn_o_n64_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_kf0.35_cnn_o_n64_xlum_0.3_z_0.1.toml"
 
 #param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_cnn_o_n32_xlum_0.3_z_0.1.toml"
 #param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_cnn_o_n16_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_cnn_o_n8_xlum_0.3_z_0.1.toml"
 
 ## PS
 
@@ -33,22 +42,54 @@ n_threads=1
 #param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n32_xlum_0.3_z_0.1.toml"
 #param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n16_xlum_0.3_z_0.1.toml"
 
-#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.5_xlum_0.3_z_0.1.toml"
-#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.33_xlum_0.3_z_0.1.toml"
-param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.27_xlum_0.3_z_0.1.toml"
-#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.25_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.8_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.6_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.4_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.37_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.36_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.34_xlum_0.3_z_0.1.toml"
+
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n32_kf0.33_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n16_kf0.33_xlum_0.3_z_0.1.toml"
+
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_kf0.35_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n32_kf0.35_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n16_kf0.35_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n8_kf0.35_xlum_0.3_z_0.1.toml"
+
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.8_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.6_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.5_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.4_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.37_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.36_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.35_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.34_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.33_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.31_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.3_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.27_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.2_xlum_0.3_z_0.1.toml"
+#param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_ps_n64_kf0.1_xlum_0.3_z_0.1.toml"
+
+########## Output dir. #########
 
 ## NC
 
-########## Output dir. #########
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_n64_xlum_0.3_z_0.1"
 
 ## CNN
 
 #output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/cnn_o_n64_xlum_0.3_z_0.1"
 #output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_cnn_o_n64_xlum_0.3_z_0.1"
 
+output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_kf0.33_cnn_o_n64_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_kf0.35_cnn_o_n64_xlum_0.3_z_0.1"
+
 #output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_cnn_o_n32_xlum_0.3_z_0.1"
 #output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_cnn_o_n16_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_cnn_o_n8_xlum_0.3_z_0.1"
 
 ## PS
 
@@ -56,12 +97,36 @@ param_file=$WORK_MLS"/codes/MLS-CNN/inputs/input_leonardo_sobol_lcdm_nc_ps_n64_k
 #output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n32_xlum_0.3_z_0.1"
 #output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n16_xlum_0.3_z_0.1"
 
-#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.5_xlum_0.3_z_0.1"
-#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.33_xlum_0.3_z_0.1"
-output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.27_xlum_0.3_z_0.1"
-#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.25_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.8_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.6_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.4_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.37_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.36_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.34_xlum_0.3_z_0.1"
 
-## NC
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n32_kf0.33_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n16_kf0.33_xlum_0.3_z_0.1"
+
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n64_kf0.35_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n32_kf0.35_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n16_kf0.35_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/nc_ps_n8_kf0.35_xlum_0.3_z_0.1"
+
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.8_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.6_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.5_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.4_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.37_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.36_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.35_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.34_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.33_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.31_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.3_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.27_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.2_xlum_0.3_z_0.1"
+#output_dir=$WORK_MLS"/results/trained_models_leonardo/sobol_lcdm/ps_n64_kf0.1_xlum_0.3_z_0.1"
 
 # Python main script.
 exe_python=$WORK_MLS"/codes/MLS-CNN/main.py"
@@ -73,11 +138,13 @@ cd $WORK_MLS/codes/MLS-CNN
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
-uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_1.log &
-uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_2.log &
-uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_3.log &
-uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_4.log &
+#uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_11.log
 
-wait
+#uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_1.log &
+#uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_2.log &
+#uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_3.log &
+#uv run python $exe_python tune -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/tune_4.log &
+
+#wait
 
 uv run python $exe_python train -n $n_threads -f $param_file -o $output_dir &> ${output_dir}/logs/train.log
